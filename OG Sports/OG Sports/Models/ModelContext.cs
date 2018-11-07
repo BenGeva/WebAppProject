024196902d49e2a1921776b7f1273ea82ac1782c
Ben <bengeva@gmail.com>
2018-11-07 20:30:32 +0200

    Add products page. need to add filters

--- OG Sports/OG Sports/Models/ModelContext.cs
+++ OG Sports/OG Sports/Models/ModelContext.cs
@@ -9,21 +9,15 @@ namespace OG_Sports.Models
 {
     public class ModelContext : DbContext
     {
-        public ModelContext() : base("name=OGSports")
+        public ModelContext() : base("OGSports")
         {
-            //Database.SetInitializer()
+            Database.SetInitializer(new InitData());
         }
-   
+
         public DbSet<Product> Products { get; set; }
-        public DbSet<Order> Ordersss { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<ProductsPerOrder> ProductsPerOrder { get; set; }
         public DbSet<ProductCategory> ProductsCategories { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            Database.SetInitializer<ModelContext>(null);
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
--- OG Sports/OG Sports/Models/ProductCategory.cs
+++ OG Sports/OG Sports/Models/ProductCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,7 @@ namespace OG_Sports.Models
         public int ProductCategoryID { get; set; }
         public string Name { get; set; }
         public string VideoPath { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }

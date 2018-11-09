165ef49f529dfcc6cc62f25528bc16bf2aebe782
ohad elal <ohadelal3@gmail.com>
2018-11-09 19:46:00 +0200

    Finish manage products view

--- OG Sports/OG Sports/Controllers/ProductController.cs
+++ OG Sports/OG Sports/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +16,111 @@ namespace OG_Sports.Controllers
         public ActionResult Index()
         { 
             return View(db.Products.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductName, Details, PicPath, Price")] Product prd)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get the selected category
+                int categoryID;
+                int.TryParse(Request["categoryID"], out categoryID);
+                ProductCategory category = db.ProductsCategories.SingleOrDefault(x => x.ProductCategoryID == categoryID);
+
+                prd.ProductCategoryID = category.ProductCategoryID;
+                prd.ProductCategory = category;
+                prd.PicPath = "/Content/Images/" + prd.PicPath + ".jpg";
+                db.Products.Add(prd);
+                db.SaveChanges();
+                return RedirectToAction("Manage", "Product");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Manage()
+        {
+            return View(db.Products.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? productId)
+        {
+            if (productId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Get the user by the id
+            Product product = db.Products.Find(productId);
+
+            if (product == null)
+            {
+                ViewBag.errorMsg = "המוצר אינו קיים במערכת, אנא רענן את העמוד";
+                return View("Manage");
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId, ProductName, Details, PicPath, Price")] Product prd)
+        {
+            if (ModelState.IsValid)
+            {
+                Product prdToUpdate = db.Products.SingleOrDefault(x => x.ProductId == prd.ProductId);
+
+                if (prdToUpdate != null)
+                {
+                    // Get the selected category
+                    int categoryID;
+                    int.TryParse(Request["categoryID"], out categoryID);
+                    ProductCategory category = db.ProductsCategories.SingleOrDefault(x => x.ProductCategoryID == categoryID);
+
+                    prdToUpdate.ProductCategoryID = category.ProductCategoryID;
+                    prdToUpdate.ProductCategory = category;
+                    prdToUpdate.ProductName = prd.ProductName;
+                    prdToUpdate.Details = prd.Details;
+                    prdToUpdate.PicPath = "/Content/Images/" + prd.PicPath + ".jpg";
+                    prdToUpdate.Price = prd.Price;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Manage");
+            }
+
+            return View(prd);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? productId)
+        {
+            // Get the user by the id and remove it from the database
+            Product prdToDelete = db.Products.Find(productId);
+
+            if (prdToDelete == null)
+            {
+                ViewBag.errorMsg = "המוצר אינו קיים במערכת, אנא רענן את העמוד";
+                return View("Manage");
+            }
+
+            db.Products.Remove(prdToDelete);
+            db.SaveChanges();
+
+            // Redirect to manage user page
+            return RedirectToAction("Manage", "Product");
         }
 
         [HttpPost]
